6976a5d2621f4d30e86cfb69487963de64c405e0
Marco Marchionni <marco.marchionni@gmail.com>
2024-04-17 11:34:51 +0200

    dropdown appears with single click

--- src/app/components/trades/trade-table/trade-table.component.ts
+++ src/app/components/trades/trade-table/trade-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   GridModule,
@@ -9,6 +9,8 @@ import {
   IEditCell,
   EditSettingsModel,
   ToolbarItems,
+  GridComponent,
+  EditEventArgs,
 } from '@syncfusion/ej2-angular-grids';
 import { DataManager, Query } from '@syncfusion/ej2-data';
 
@@ -21,35 +23,70 @@ import { DataManager, Query } from '@syncfusion/ej2-data';
   providers: [EditService, ToolbarService, PageService, CommandColumnService],
 })
 export class TradeTableComponent implements OnInit {
-  @Input() tableData!: DataManager;
-  @Input() strategies!: DataManager;
-  public strategyParams!: IEditCell;
-
-  ngOnInit(): void {
-    this.updateStrategyParams();
-  }
-
-  updateStrategyParams() {
-    this.strategyParams = {
-      params: {
-        dataSource: this.strategies,
-        fields: { value: 'name', text: 'name' },
-        query: new Query(),
-        actionComplete: () => false,
-      },
-    };
-  }
-
+  @Input() tableData?: DataManager;
+  @Input() strategies?: DataManager;
+  @ViewChild('grid')
+  public grid?: GridComponent;
+  public strategyParams?: IEditCell;
   public editSettings: EditSettingsModel = {
     allowEditing: true,
     allowAdding: false,
     allowDeleting: false,
     mode: 'Batch',
   };
+  public toolbar: ToolbarItems[] = ['Update', 'Cancel'];
+  private lastClickedCellInfo: {
+    index: number;
+    colIndex: number;
+    field?: string;
+  } | null = null;
 
-  public toolbar: ToolbarItems[] = ['Edit', 'Update', 'Cancel'];
+  ngOnInit(): void {
+    this.strategyParams = {
+      params: {
+        dataSource: this.strategies,
+        fields: { value: 'name', text: 'name' },
+        query: new Query(),
+        actionComplete: ($event) => {
+          console.log('ActionComplete', $event);
+          this.showDropDown();
+        },
+      },
+    };
+  }
 
-  onStrategyUpdate($event: any) {
-    console.log($event);
+  onClick(args: any) {
+    console.log('Click', args);
+    if (args?.target?.classList?.contains('e-rowcell')) {
+      var index = parseInt(args.target.getAttribute('Index'));
+      var colIndex = parseInt(args.target.getAttribute('aria-colindex')) - 1;
+      var field = (this.grid as GridComponent).getColumns()[colIndex].field;
+      console.log('Index', index, 'ColIndex', colIndex, 'Field', field);
+      if (field === 'strategy') {
+        this.lastClickedCellInfo = { index, colIndex, field };
+        console.log('Set LastClickedCellInfo', this.lastClickedCellInfo);
+        (this.grid as GridComponent).editModule.editCell(index, field);
+      } else {
+        console.log('Field is not strategy');
+      }
+    }
+  }
+
+  showDropDown() {
+    if (this.lastClickedCellInfo) {
+      console.log('LastClickedCellInfo', this.lastClickedCellInfo);
+      const cell = this.grid?.getCellFromIndex(
+        this.lastClickedCellInfo?.index,
+        this.lastClickedCellInfo?.colIndex
+      );
+      console.log('Cell', cell);
+      const dropDownElement = cell?.querySelector('.e-dropdownlist');
+      console.log('DropDownElement', dropDownElement);
+      if (dropDownElement) {
+        const dropDownInstance = (dropDownElement as any).ej2_instances[0];
+        dropDownInstance.showPopup();
+      }
+    }
+    this.lastClickedCellInfo = null;
   }
 }
