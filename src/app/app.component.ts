871a7aa4ff617b98b63a1c515c729eb11fa229a6
Marco Marchionni <marco.marchionni@gmail.com>
2024-10-09 19:30:45 +0200

    Fixed height issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthViewComponent } from './components/auth/auth-view/auth-view.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -19,7 +19,7 @@ import { AuthService } from './services/auth-service/auth.service';
     AuthViewComponent,
   ],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = 'strategist-app';
   @ViewChild(SidenavComponent) sidenav?: SidenavComponent;
   @ViewChild(HeaderComponent) header?: HeaderComponent;
@@ -29,20 +29,6 @@ export class AppComponent implements AfterViewInit {
     this.authService.isAuthenticated$.subscribe((auth) => {
       this.isAuthenticated = auth;
     });
-  }
-
-  ngAfterViewInit(): void {
-    this.adjustMainContentHeight();
-    window.addEventListener('resize', () => this.adjustMainContentHeight());
-  }
-
-  private adjustMainContentHeight() {
-    if (!this.header) {
-      return; // If header is not defined, do nothing
-    }
-    const headerHeight = (this.header as HeaderComponent).getAppBarHeight();
-    const mainContent = document.querySelector('.main-content') as HTMLElement;
-    mainContent.style.minHeight = `calc(100vh - ${headerHeight}px)`;
   }
 
   toggleSidenav() {
